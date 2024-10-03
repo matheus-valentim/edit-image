2faa741c1ad19536696c569bfac083dc6c27ad7c
matheus figueiredo valentim farias <64991477+matheus-valentim@users.noreply.github.com>
2024-10-03 19:09:21 -0300

    edit image

--- src/src/main/java/imagem/edit/dto/ImagemDto.java
+++ src/src/main/java/imagem/edit/dto/ImagemDto.java
@@ -1,15 +1,9 @@
 package imagem.edit.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.JsonNode;
+
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Null;
-
-import java.util.List;
-import java.util.UUID;
 
 
 public record ImagemDto(@NotBlank String nome,@NotNull ObjectNode transformations) {
